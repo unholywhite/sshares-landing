ab5d75192f11fe12a3170b50a50a71d56363cda7
unholywhite <s.krupnyk@gmail.com>
2024-04-23 04:34:18 +0300

    typography fixes

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -11,8 +11,11 @@ const HomePage: NextPage = () => {
   return (
     <MainLayout>
       <LogoDark className={s.logo} />
-      <h1 className={s.title}>Secure Shares</h1>
-      <p className={s.text}>The First AI-Powered RWA Marketplace</p>
+      <h1 className={s.title}>The First AI-Powered RWA Marketplace</h1>
+      <p className={s.text}>
+        Secure Shares removes all barriers for investing in cash-flow-generating
+        blue-chip startups and SMBs.
+      </p>
       <div className={s.icons}>
         <Icon name="email" />
         <Icon name="socialX" />
